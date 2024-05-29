dd66ea949cc082015554808b6b1baf9b229228ef
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-05-29 10:53:44 +0000

    1. Log formatter Update the class to accept a list of strings fields constructor argument.
    Implement the format method to filter values in incoming log records using filter_datum. Values for fields in fields should be filtered.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 """Personal data"""
+import logging
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
     pattern = '|'.join([f'(?<={field}=)[^{separator}]+' for field in fields])
     return re.sub(pattern, redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == "__main__":
+    main()
