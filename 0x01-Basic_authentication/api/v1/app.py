11d3a53239e0aa0806964967a3ab49f2a4f97b72
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-06-04 12:10:44 +0000

    5. Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,11 +2,10 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
