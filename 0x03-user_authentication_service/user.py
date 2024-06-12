7c6106de4aabff456f52c4cba8b93a4bd9f9b16b
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-06-12 18:21:57 +0000

    1. create user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,7 +9,6 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
